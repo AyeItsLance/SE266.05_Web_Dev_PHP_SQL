edaaacb855e5c5b57e988aa5af96de540970a4f3
Lespinasse <008010258@N358-B8.local>
2021-10-05 14:53:22 -0400

    update

---
+++ w1_2/index.php
@@ -0,0 +1,40 @@
+<?php
+
+
+$persons = [
+
+    'age' => 31,
+
+    'hair color' => 'brown',
+
+    'sport' => 'basketball'
+
+];
+
+
+//creating a push to my og array
+
+$persons['name'] = 'Lance';
+
+//getting rid of an item from array
+
+unset($persons['age']);
+
+
+//homework create a task associate array
+
+$task = [      
+
+
+];
+
+//title of task, when it's due, who needs to do it, and if its completed (yes or no strings for now)
+
+
+
+
+#the values to my left are my keys values
+
+require 'index.view.php';
+
+#making sure the view and index are connected
