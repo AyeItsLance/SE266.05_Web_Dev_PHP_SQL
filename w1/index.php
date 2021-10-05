646cc4b231b3d4bbb10f9717b925ddd1129c14b8
lance lespinasse <buttonmasher123@yahoo.com>
2021-10-05 02:01:50 -0400

    Final Update

--- w1/index.php
+++ w1/index.php
@@ -21,7 +21,7 @@ $animals = [
 
 
 
-require 'w1\inedx.view.php';
+require 'C:\Users\James\OneDrive\DesktopNEIT Stuff\TERM 5\WebDev With PHP\SE266.05_Web_Dev_PHP_SQL\w1\inedx.view.php';
 
 
 //making my index file require my other index file
