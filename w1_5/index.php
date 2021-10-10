59d4c8a5c423a52c3bbd840e5eafdb7403e708a0
lance lespinasse <buttonmasher123@yahoo.com>
2021-10-10 19:32:07 -0400

    update

--- w1_5/index.php
+++ w1_5/index.php
@@ -1,5 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-require 'index.view.php';
+    <?php
 
 
+
+
+        require 'index.php';
+
+        for( $i=1; $i<=10; $i++ )
+        {
+
+            echo $i;
+            echo "<br>";
+
+        }
+
+        
+
+    ?>
+    
+</body>
+</html>
