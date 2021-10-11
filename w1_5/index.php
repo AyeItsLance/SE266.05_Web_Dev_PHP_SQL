41b576d6099db838160692162fb44e67e2de865d
lance lespinasse <buttonmasher123@yahoo.com>
2021-10-10 20:03:05 -0400

    update

--- w1_5/index.php
+++ w1_5/index.php
@@ -26,11 +26,10 @@
                 <print>buzz</print>
                 <br> <!--If my number can be divided by 3 it means that the number can be a multiple of 3-->
             
-            <?php elseif($num % 6 == 0) :?>
+            <?php elseif($num % 6 == 0) 
 
-                <print>fizz buzz</print>
-                <br>
-                <!--If my number can be divided by 6 it means that the number can be a multiple of 2 and 3 together-->
+                echo $num
+                :?>
             <?php else :?>
 
                 <print>$num</print>
