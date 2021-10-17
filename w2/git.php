bc978ad47b805722db18c96c4e53d77f24f7b41b
lance lespinasse <buttonmasher123@yahoo.com>
2021-10-17 04:48:04 -0400

    update

--- w2/git.php
+++ w2/git.php
@@ -66,7 +66,8 @@
 
         <hr>
 
-        <?php       
+        <?php   
+        date_default_timezone_set("America/New_York");    
         $file = basename($_SERVER['PHP_SELF']);
         $mod_date=date("F d Y h:i:s A", filemtime($file));
         echo "File last updated $mod_date ";
