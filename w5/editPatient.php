36b13636f4df7eb14685b62f6d5e5fdf581129c1
lance lespinasse <buttonmasher123@yahoo.com>
2021-11-08 10:05:47 -0500

    update

---
+++ w5/editPatient.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Patient</title>  
+</head>
+<body>
+
+    <?php
+
+        //COPIED OVER FROM WEEK 4
+        include __DIR__ . '/functions.php';
+        include __DIR__. '/model_patient.php';
+        //setting up some functions for later use
+        function age ($bdate) {
+            $date = new DateTime($bdate);
+            $now = new DateTime();
+            $interval = $now->diff($date);
+            return $interval->y;
+        }
+        
+        function isDate($dt) {
+            try {
+            $d = new DateTime($dt);
+            return (true);
+            } catch(Exception $e) {
+            return false;
+            }
+        }
+
+
+
+
+
+
+        $firstName = filter_input(INPUT_POST, 'fName');         //having these up top to force the program to stick after entering it in
+
+        $lastName = filter_input(INPUT_POST, 'lName');
+
+        $marriedYes = filter_input(INPUT_POST, 'marriedYes');       
+
+        $marriedNo = filter_input(INPUT_POST, 'marriedNo');     
+
+        $age = filter_input(INPUT_POST, 'birthDate');     
+        
+        
+        $action = filter_input(INPUT_GET, 'action');
+
+        $id = filter_input(INPUT_GET, 'patientId');
+
+
+        $row = getPatients($id);
+
+        $firstName = $row['patientFirstName'];
+
+        $lastName = $row['patientLastName'];
+
+        $status = $row['patientMarried'];
+
+        $age = $row['patientBirthDate'];
+
+        
+
+    ?>
+
+    <h1>Please Sign This Form</h1>
+
+    <!--ALL MY FORM INFORMATIONS-->
+
+    <form action='editPatient.php' method='post'>
+
+
+        <input type="text" name="action" value="<?= $action;?>">
+
+        <br>
+        <br>
+
+
+        <label><strong>ID Number</strong></label>
+        <input type="text" name="teamId" value="<?= $id;?>">
+
+        <br>
+        <br>
+
+        <label><strong>First Name</strong></label>
+        <input type='text' name='fName' placeholder='First Name' value="<?= $firstName; ?>" />
+
+        <label><strong>Last Name</strong></label>
+        <input type='text' name='lName' placeholder='Last Name' value="<?= $lastName; ?>"/>
+
+        <br>
+        <br>
+
+        <label for='married'><strong>Married?</strong></label><br>
+
+        <input type='radio' name='marriedYes'<?php if($marriedYes != '') echo 'Checked'; ?> >
+        <label for ='mYes'>Yes</label>
+        
+        <br>
+
+        <input type='radio' name='marriedNo' <?php if($marriedNo != '') echo 'Checked'; ?>>
+        <label for ='mNo'>No</label>
+        
+        <br>
+        <br>
+
+        <label><strong>Date of Birth</strong></label>
+        <input type='date' name='birthDate' value="<?= $age; ?>" />
+
+        <br>
+
+
+
+
+        <br>
+        <button type='submit' name='submitBtn'> <?php echo $action; ?> Patient </button>
+
+        <br>
+
+        <a href="index.php">Return to Patient List</a>
+
+
+       
+
+    </form> <!--END OF FORM-->
+
+
+    <?php   
+
+        $error1 = 0; //creating a error var
+        $error2 = 0; //creating a error var
+        $error3 = 0; //creating a error var
+        $error4 = 0; //creating a error var
+        $error5 = 0; //creating a error var         all of these error vars are for different areas which will force a user to enter everything in before the information at the end displays
+
+        //first we will be setting up the first and last names
+
+
+
+        if(isset($_POST['submitBtn'])) {        //all of my code will activate once my submit btn is pressed
+
+            echo '<hr/> Form Submited <br/>';
+
+            $firstName = filter_input(INPUT_POST, 'fName');                 //grabbing my first name and storing it
+
+            if($firstName == ''){
+
+                echo 'Make sure your first name is a real name!<br>';       //making sure my first name has a value
+
+                $error1 = 1;     //what these errors are doing is storing the value this is imporant later
+
+            } 
+            else{
+
+                $error1 = 0; //what these errors are doing is storing the value this is imporant later
+            }
+
+            $lastName = filter_input(INPUT_POST, 'lName');      //grabbing my last name and storing it
+
+            if($lastName == ''){
+
+                echo 'Make sure your last name is a real name!<br>';        //making sure my last name has a value
+
+                $error2 = 1;
+
+            } else{
+
+                $error2 = 0;
+            }
+
+            //after grabbing my first and last names fields I store it into Full name which is at the bottom
+
+
+            //now on to marriage
+
+
+            $marriedYes = filter_input(INPUT_POST, 'marriedYes');       //storing the yes value
+
+            $marriedNo = filter_input(INPUT_POST, 'marriedNo');     //storing the no value
+
+            if($marriedYes != '')
+            {
+                $status = 1;        //1 for true
+            }
+
+            if($marriedNo != '')
+            {
+                $status = 0;        //0 for false
+            }
+
+
+
+
+            if($marriedYes == '' and $marriedNo == ''){
+
+                echo 'Make sure you select your mariage status! <br>';      //if neither yes or no are filled out will print error
+
+                $error3 = 1;
+
+                
+
+            } else{
+
+                $error3 = 0;
+            }
+
+
+            //my set up for age
+
+
+
+            $age = filter_input(INPUT_POST, 'birthDate');
+
+
+            if($age == '')
+            {
+                echo 'Please select an age! <br>';
+
+                $error4 = 1;
+            } else{
+
+                $error4 = 0;
+            }
+
+
+            
+
+
+
+
+           
+
+    
+
+
+           
+
+            if($error1 == 0 and $error2 == 0 and $error3 == 0 and $error4 == 0)     //at the end if my error is 0 that means no errors occured throughout my program and im all set to continue as usal
+            {
+
+                //displaying my data along with the else statement
+
+                
+
+                $action = filter_input(INPUT_GET, 'action');
+
+                $id = filter_input(INPUT_GET, 'patientId');
+
+            
+
+                
+
+                echo 'Full Name: ', $firstName, ' ', $lastName, '<br>';
+
+                echo 'Date of Birth: ', $age, '<br>';
+
+                
+                if($status == false)
+                {
+                    echo 'Married: No<br>';
+                }
+                
+                else{       //status will come in later, this is what is stored and sent to the db if false will equal 0 and true will equal 1
+
+                    echo 'Married: Yes<br>';
+                }
+
+                if (isPostRequest()) {
+
+                    
+                    $result = updatePatient ($id, $firstName, $lastName, $status, $age);
+                    
+                    echo "Paitent Updated";
+                }
+
+            
+
+                //creating my class system
+
+    
+            }
+            else
+            {       //if error equals anything else this means that something happened along the way and I need to go back and correct
+
+                echo 'Please fix your errors';
+            }
+
+           
+
+        } else{
+            echo '<hr/>Loading information';
+        }
+
+       
+
+
+    ?>
+    
+    
+</body>
+</html>
---
+++ w5/model_patient.php
@@ -0,0 +1,122 @@
+<?php
+
+
+include (__DIR__ . '/db.php'); //grabbing my db file
+
+function GetPatient () {     //creating my function that grabs my paitents
+    global $db;    //global database
+
+
+    $results = [];      //creating my empty resuts array
+
+    $stmt = $db->prepare("SELECT id, patientFirstName, patientLastName, patientMarried, patientBirthDate FROM patients ORDER BY patientLastName");      //this is my sql code that selects my paitent
+
+
+    if ( $stmt->execute() && $stmt->rowCount() > 0) {
+
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+    }        //this is me checking to make sure the code executes
+
+    return ($results);      //returning my results var
+
+}
+
+
+function addPatient ($f, $l, $s, $a)  {
+    
+    //craeting my add patient function that will actually add to my db
+
+
+    global $db;
+
+    $results = "Not addded";        //this will display if code doesnt work
+
+    $stmt = $db->prepare("INSERT INTO patients SET patientFirstName = :firstname, patientLastName = :lastname, patientMarried = :status, patientBirthDate = :age ");     //craeting my sql statement that will add data into the db
+
+    $binds = array(
+        ":firstname" => $f,
+        ":lastname" => $l,
+        ":status" => $s,
+        ":age" => $a        //binding my information of array to my vars
+    );
+
+
+    if ($stmt->execute($binds) && $stmt->rowCount() > 0) {
+        $results = "Patient Added";     //if command works print out patient added
+    }
+
+}
+
+
+//creating my update or edit patient
+
+function updatePatient($id, $firstname, $lastname, $status, $age){
+
+    global $db;
+
+    $results = "";      //creating empty var
+
+    $stmt = $db->prepare("UPDATE patients SET patientFirstName = :firstname, patientLastName = :lastname, patientMarried = :status, patientBirthDate = :age WHERE id=:id");       //my update statement
+
+
+    $stmt->bindValue(':id', $id);
+    $stmt->bindValue(':firstname', $firstname);     //binding all my values
+    $stmt->bindValue(':lastname', $lastname);
+    $stmt->bindValue(':status', $status);
+    $stmt->bindValue(':age', $age);
+
+    if ($stmt->execute() && $stmt->rowCount() > 0) {
+
+        $results = 'Patient Updated';      //if stmt executes then Data updated will print to my screen
+
+    }
+    
+    return ($results);      //results
+}
+
+
+function deletePatient ($id) {     //creating my function for deleting patients
+    global $db;     //
+    
+    $results = "Patient was not deleted";      //creating my results var
+
+    $stmt = $db->prepare("DELETE FROM patients WHERE id=:id");      //so if the id is the same as the id im editing it will delete 
+    
+    $stmt->bindValue(':id', $id);
+        
+    if ($stmt->execute() && $stmt->rowCount() > 0) {
+
+        $results = 'Patient Deleted';       //if stmt works then patient deleted will be printed
+
+    }
+    
+    return ($results);      //returning my results
+}
+
+function getPatients ($id) {
+
+    global $db;
+
+   
+   $result = [];        //creating empty array
+   
+   $stmt = $db->prepare("SELECT id, patientFirstName, patientLastName, patientMarried, patientBirthDate FROM patients WHERE id=:id");
+
+   $stmt->bindValue(':id', $id);
+  
+
+   if ( $stmt->execute() && $stmt->rowCount() > 0 ) {
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                   
+    }
+    
+    return ($result);
+}
+
+
+
+
+?>
