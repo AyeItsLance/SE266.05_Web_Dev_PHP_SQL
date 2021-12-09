68d6c5f301f91c888d1a6298e34bbfaa99eb47a9
lance lespinasse <buttonmasher123@yahoo.com>
2021-12-09 08:59:59 -0500

    update

--- w9/header.php
+++ w9/header.php
@@ -32,10 +32,6 @@
     <div class="navbar-header">
       <span class="navbar-brand">Schools</span>
     </div>
-    <ul class="nav navbar-nav">
-        <li <?php if(basename($_SERVER['PHP_SELF']) == 'upload.php'): ?>class="active"<?php endif; ?>><a href="upload.php">Upload</a></li>
-        <li <?php if(basename($_SERVER['PHP_SELF']) == 'search.php'): ?>class="active"<?php endif; ?>><a href="search.php">Search</a></li>
-    </ul>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
     </ul>
